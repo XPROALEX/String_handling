86047e5d020532a359ce32d252bbfecbb91eab72
Alex <alex97av@gmail.com>
2023-07-14 16:50:09 +0200

    compilazione esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.lang.reflect.Array;
+
 public class Main {
     public static void main (String[]args){
-        String string1="hello";
+        String string1="Hello";
         String string2="How are you?";
+        String firstTwoWord=string1.substring(0,2) ;
+        String lastTwoWord=string2.substring(string2.length()-2);
+        String charArray = firstTwoWord+lastTwoWord;
+        System.out.println(charArray);
     }
 }
