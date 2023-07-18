979852c1d9046b151c8f2f1dbb2078509fe1f1af
Alessandro <alex97av@gmail.com>
2023-07-18 18:53:54 +0200

    modifica e correzione esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import java.io.CharArrayReader;
 import java.lang.reflect.Array;
 
 public class Main {
     public static void main (String[]args){
         String string1="Hello";
         String string2="How are you?";
-        String firstTwoWord=string1.substring(0,2) ;
-        String lastTwoWord=string2.substring(string2.length()-2);
-        String charArray = firstTwoWord+lastTwoWord;
-        System.out.println(charArray);
+        String firstAndLastTwoWord=string1.substring(0,2)+string2.substring(string2.length()-2) ;
+        char[] charArray = firstAndLastTwoWord.toCharArray();
+         System.out.println(charArray);
     }
 }
