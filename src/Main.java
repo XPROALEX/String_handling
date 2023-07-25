37822d9d0639f20b5578fa23f4b4693b45f1cde5
Alex <alex97av@gmail.com>
2023-07-25 13:59:23 +0200

    print array

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.io.CharArrayReader;
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main (String[]args){
         String string1="Hello";
         String string2="How are you?";
-        char[] charArray= (string1.substring(0,2)+string2.substring(string2.length()-2)).toCharArray();
-         System.out.println(charArray);
+        char[] charArray=new char[4];
+        charArray= (string1.substring(0,2)+string2.substring(string2.length()-2)).toCharArray();
+
+         System.out.println(Arrays.toString(charArray));
     }
 }
